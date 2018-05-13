98b4046f95a851a87bdcfd3ce117d25033c4f173
Quang Ngô <ngoquang2708@gmail.com>
2018-05-13 13:54:02 +0700

    Move proprietary stuffs to vendor

--- vivalto3gvn-vendor-blobs.mk
+++ vivalto3gvn-vendor-blobs.mk
@@ -1,67 +1,50 @@
-#
-# Copyright (C) 2016 Android Open Source Project
-# Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-PROPRIETARY_FILES := \
-	bin/at_distributor \
-	bin/engpc \
-	bin/ext_data.sh \
-	bin/ext_kill.sh \
-	bin/ext_symlink.sh \
-	bin/gps.cer \
-	bin/gpsd \
-	bin/macloader \
-	bin/modemd \
-	bin/nvitemd \
-	bin/phoneserver \
-	bin/refnotify \
-	bin/rild \
-	lib/egl/libEGL_mali.so \
-	lib/egl/libGLESv1_CM_mali.so \
-	lib/egl/libGLESv2_mali.so \
-	lib/hw/camera2.scx15.so \
-	lib/hw/camera.scx15.so \
-	lib/hw/gps.default.so \
-	lib/hw/sensors.scx15.so \
-	lib/libatchannel.so \
-	lib/libatparser.so \
-	lib/libboost.so \
-	lib/libfactoryutil.so \
-	lib/libisp.so \
-	lib/libMali.so \
-	lib/libmorpho_easy_hdr.so \
-	lib/libmorpho_facesolid.so \
-	lib/libomission_avoidance.so \
-	lib/libomx_aacdec_sprd.so \
-	lib/libomx_avcdec_hw_sprd.so \
-	lib/libomx_avcdec_sw_sprd.so \
-	lib/libomx_avcenc_hw_sprd.so \
-	lib/libomx_m4vh263dec_hw_sprd.so \
-	lib/libomx_m4vh263dec_sw_sprd.so \
-	lib/libomx_m4vh263enc_hw_sprd.so \
-	lib/libomx_mp3dec_sprd.so \
-	lib/libomx_vpxdec_hw_sprd.so \
-	lib/libreference-ril_sp.so \
-	lib/libril.so \
-	lib/librilutils.so \
-	lib/libsecnativefeature.so \
-	lib/libsecril-client.so \
-	lib/libsprd_agps_agent.so \
+__vendor_files := \
+	vendor/bin/at_distributor \
+	vendor/bin/engpc \
+	vendor/bin/ext_data.sh \
+	vendor/bin/ext_kill.sh \
+	vendor/bin/ext_symlink.sh \
+	vendor/bin/gps.cer \
+	vendor/bin/gpsd \
+	vendor/bin/macloader \
+	vendor/bin/modemd \
+	vendor/bin/nvitemd \
+	vendor/bin/phoneserver \
+	vendor/bin/refnotify \
+	vendor/bin/rild \
+	vendor/lib/egl/libEGL_mali.so \
+	vendor/lib/egl/libGLESv1_CM_mali.so \
+	vendor/lib/egl/libGLESv2_mali.so \
+	vendor/lib/hw/camera2.scx15.so \
+	vendor/lib/hw/camera.scx15.so \
+	vendor/lib/hw/gps.default.so \
+	vendor/lib/hw/sensors.scx15.so \
+	vendor/lib/libatchannel.so \
+	vendor/lib/libatparser.so \
+	vendor/lib/libboost.so \
+	vendor/lib/libfactoryutil.so \
+	vendor/lib/libisp.so \
+	vendor/lib/libMali.so \
+	vendor/lib/libmorpho_easy_hdr.so \
+	vendor/lib/libmorpho_facesolid.so \
+	vendor/lib/libomission_avoidance.so \
+	vendor/lib/libomx_aacdec_sprd.so \
+	vendor/lib/libomx_avcdec_hw_sprd.so \
+	vendor/lib/libomx_avcdec_sw_sprd.so \
+	vendor/lib/libomx_avcenc_hw_sprd.so \
+	vendor/lib/libomx_m4vh263dec_hw_sprd.so \
+	vendor/lib/libomx_m4vh263dec_sw_sprd.so \
+	vendor/lib/libomx_m4vh263enc_hw_sprd.so \
+	vendor/lib/libomx_mp3dec_sprd.so \
+	vendor/lib/libomx_vpxdec_hw_sprd.so \
+	vendor/lib/libreference-ril_sp.so \
+	vendor/lib/libril.so \
+	vendor/lib/librilutils.so \
+	vendor/lib/libsecnativefeature.so \
+	vendor/lib/libsecril-client.so \
+	vendor/lib/libsprd_agps_agent.so \
 	vendor/firmware/BCM4343A0_001.001.034.0038.0090_ORC_Vivalto-3G_3MC.hcd \
 	vendor/firmware/vbc_eq \
 
 PRODUCT_COPY_FILES += \
-	$(foreach f,$(PROPRIETARY_FILES),vendor/samsung/vivalto3gvn/proprietary/$(f):system/$(f))
+	$(foreach f,$(__vendor_files),vendor/samsung/vivalto3gvn/proprietary/$(f):system/$(f))
